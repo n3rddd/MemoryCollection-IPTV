bed7dd413adf5b18a011e08b2b838f5fa404c0c1
dong <csszue@gmail.com>
2024-11-08 22:13:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-import json
-import requests
-import socket
-from bs4 import BeautifulSoup
 import re
 import os
-import threading
-from concurrent.futures import ThreadPoolExecutor
-from queue import Queue, Empty
-from datetime import datetime
+import json
 import time
+import socket
+import requests
+import threading
 from github import Github
+from datetime import datetime
+from bs4 import BeautifulSoup
+from queue import Queue, Empty
 
+def read_json_file(file_path):
+    """
+    读取 JSON 文件内容并返回字典数据。
+    如果文件不存在，返回 None。
+    """
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
+    except Exception as e:
+        print(f"读取文件失败: {e}")
+        return None
 
 def write_json_file(file_path, data, overwrite=False):
     """
@@ -21,6 +33,7 @@ def write_json_file(file_path, data, overwrite=False):
     - overwrite: 为 True 时覆盖文件，为 False 时追加数据。
     """
     if overwrite:
+        # 如果需要覆盖文件，直接写入数据
         try:
             with open(file_path, 'w', encoding='utf-8') as file:
                 json.dump(data, file, ensure_ascii=False, indent=4)
@@ -28,41 +41,32 @@ def write_json_file(file_path, data, overwrite=False):
         except Exception as e:
             print(f"覆盖写入文件失败: {e}")
     else:
+        # 如果需要追加数据，先读取现有文件
         existing_data = read_json_file(file_path)
         
-        # 如果读取到的现有数据为空，则初始化为默认数据结构
         if not existing_data:
-            existing_data = {"详情": {"ip": [], "iptv": 0}, "直播": {}}
+            # 如果文件为空或不存在，初始化数据结构
+            existing_data = {"详情": {"iptv": 0, "ip": []}, "直播": {}}
 
-        # 更新“详情”字段：追加新的 IP 和累加频道总数
+        # 处理 IP 地址
         new_ips = [ip for ip in data["详情"]["ip"] if ip not in existing_data["详情"]["ip"]]
         existing_data["详情"]["ip"].extend(new_ips)
         existing_data["详情"]["iptv"] += data["详情"]["iptv"]
-        
-        # 合并“直播”字段
+
+        # 处理频道数据，避免重复添加
         for ip, channels in data["直播"].items():
             if ip not in existing_data["直播"]:
                 existing_data["直播"][ip] = channels
             else:
-                # 如果 IP 已存在，追加频道数据
                 existing_data["直播"][ip].extend([channel for channel in channels if channel not in existing_data["直播"][ip]])
 
+        # 写入合并后的数据
         try:
             with open(file_path, 'w', encoding='utf-8') as file:
                 json.dump(existing_data, file, ensure_ascii=False, indent=4)
             print(f"数据已追加写入到 {file_path}")
         except Exception as e:
             print(f"追加写入文件失败: {e}")
-
-def read_json_file(file_path):
-    """读取 JSON 文件内容并返回数据。如果文件不存在或内容无效，返回一个空的默认结构。"""
-    if os.path.exists(file_path):
-        try:
-            with open(file_path, 'r', encoding='utf-8') as file:
-                return json.load(file)
-        except Exception as e:
-            print(f"读取 JSON 文件失败: {e}")
-    return {"详情": {"ip": [], "iptv": 0}, "直播": {}}
 
 def check_ip_port(ip_port):
     """检查 IP 和端口是否可连接，返回 True 表示可用，False 表示不可用。"""
@@ -88,14 +92,14 @@ def get_ip(area):
 
     base_url = "http://www.foodieguide.com/iptvsearch/hoteliptv.php"
     
-    ip_list = set()  # 用于存储所有有效的 IP 地址
+    ip_list = set()  
 
-    for area_name in area:  # 遍历每个地区
+    for area_name in area:  
         data = {"saerch": area_name, "Submit": "+", "names": "Tom", "city": "HeZhou", "address": "Ca94122"}
 
         try:
             response = requests.post(base_url, headers=headers, data=data, timeout=10)
-            response.raise_for_status()  # 检查请求是否成功
+            response.raise_for_status()  
         except requests.RequestException as e:
             return {'ip_list': [], 'error': f"请求失败: {e}"}
 
@@ -103,24 +107,21 @@ def get_ip(area):
         results = soup.find_all("div", class_="result")
 
         for result in results:
-            # 查找 IP 链接
             ip_link = result.find("a", href=re.compile(r"hotellist\.html\?s="))
             if ip_link:
                 ip_address = re.search(r"s=([\d.]+:\d+)", ip_link['href'])
                 if ip_address:
-                    # 检查状态
                     status_div = result.find("div", style="color: crimson;")
                     if status_div and "暂时失效" in status_div.get_text():
-                        continue  # 跳过失效的 IP
-                    ip_list.add(ip_address.group(1))  # 添加有效 IP
+                        continue  
+                    ip_list.add(ip_address.group(1))  
     
     return {'ip_list': list(ip_list), 'error': None}
-    {'ip_list': ['59.44.203.42:9901', '175.150.152.34:9005', '218.24.193.146:808', '117.72.36.109:9099', '60.16.18.243:4949', '59.47.118.242:801', '175.190.127.179:4949'], 'error': None}
-
+  
 def get_iptv(ip_list, output_file="data/Origfile.json", overwrite=False):
     """爬取频道信息，并返回按 IP 分组的频道数据"""
     
-    ip_data = {"详情": {"ip": [], "iptv": 0}, "直播": {}}
+    ip_data = {"详情": {"iptv": 0,"ip": []}, "直播": {}}
 
     for ip in ip_list:
         existing_data = read_json_file(output_file)
@@ -163,76 +164,59 @@ def get_iptv(ip_list, output_file="data/Origfile.json", overwrite=False):
         else:
             print(f"请求失败，状态码: {response.status_code}，IP: {ip}")
 
-    # 写入或追加数据
     write_json_file(output_file, ip_data, overwrite=overwrite)
-    
     return ip_data
 
 def filter_and_process_channel_data(ip_data, output_file="data/itv.json"):
     """对频道数据进行过滤和处理，并将结果写入到指定文件"""
 
-    # 加载 db.json 配置文件
     db_config = read_json_file("data/db.json")["data"]
 
-    # 检查 db_config 是否有效
     if not db_config or "keywords" not in db_config or "discard_keywords" not in db_config or "replace_keywords" not in db_config:
         print("错误：db_config 配置缺失必要字段")
         return None
 
     try:
-        # 读取 db_config 中的关键词和替换规则
         keywords = [kw.lower() for kw in db_config["keywords"]]
         discard_keywords = [dk.lower() for dk in db_config["discard_keywords"]]
         replace_keywords = {k.lower(): v for k, v in db_config["replace_keywords"].items()}
 
-        processed_data = {"详情": {"ip": [], "iptv": 0}, "直播": {}}
-        channel_count = 0  # 初始化频道数量计数器
+        processed_data = {"详情": {"iptv": 0,"ip": []}, "直播": {}}
+        channel_count = 0  
 
-        # 遍历 ip_data["直播"]
         for ip, channels in ip_data["直播"].items():
-            ip_channels = []  # 存储过滤后的频道信息
+            ip_channels = [] 
 
-            # 遍历该 IP 下的所有频道
             for channel in channels:
                 if len(channel) == 3:
                     channel_name, url, speed = channel
 
-                    # 应用替换规则
                     for k, v in replace_keywords.items():
-                        channel_name = re.sub(re.escape(k), v, channel_name, flags=re.IGNORECASE)
+                        channel_name = re.sub(k, v, channel_name, flags=re.IGNORECASE)
 
-                    # 过滤包含不需要的关键词的频道名称
+                    channel_name = channel_name.upper()
+
                     if any(dk in channel_name.lower() for dk in discard_keywords):
                         continue
 
-                    # 保留包含指定关键词的频道名称
                     if keywords and not any(kw in channel_name.lower() for kw in keywords):
                         continue
 
-                    # 特殊处理：清除括号内容和“高清”等词
-                    channel_name = re.sub(r"（.*?）", "", channel_name)
-                    channel_name = re.sub(r"高清|超清", "", channel_name).strip()
-
-                    # 处理 URL，确保以 "http://" 开头
                     url = re.sub(r"(^http://|[ #])", "", url)
                     url = "http://" + url if not url.startswith("http://") else url
 
-                    # 添加频道信息
                     ip_channels.append([channel_name, url, speed])
-                    channel_count += 1  # 每添加一个频道，增加频道计数
+                    channel_count += 1  
 
             if ip_channels:
                 processed_data["直播"][ip] = ip_channels
-                processed_data["详情"]["ip"].append(ip)  # 添加 IP 到详情的 IP 列表
+                processed_data["详情"]["ip"].append(ip)  
 
-        # 设置 iptv 的值
-        processed_data["详情"]["iptv"] = channel_count  # 频道总数
+        processed_data["详情"]["iptv"] = channel_count  
 
-        # 替换处理 'iptv' 字段，假设需要做一些修改
         if "iptv" in processed_data["详情"]:
             processed_data["详情"]["iptv"] = str(processed_data["详情"]["iptv"])  # 示例修改：将频道总数转换为字符串
 
-        # 使用 write_json_file 覆盖写入处理结果
         write_json_file("data/itv.json",processed_data, True)  
         
         print(f"处理结果已保存到 {output_file}")
@@ -242,25 +226,7 @@ def filter_and_process_channel_data(ip_data, output_file="data/itv.json"):
         print(f"处理失败: {e}")
         return None
 
-
-    """同步多线程对 IP 和频道进行测试，并将有效结果写入 JSON 文件。"""
-    result_data = {"详情": {"ip": [], "iptv": 0}, "直播": {}}
-    with ThreadPoolExecutor(max_workers=5) as executor:  # 设置最大线程数
-        futures = []
-        for ip, channels in data.items():
-            futures.append(executor.submit(process_ip_channels, ip, channels, result_data["直播"]))
-
-        for future in futures:
-            future.result()  # 等待每个线程执行完成
-
-    # 统计有效 IP 和频道数量
-    result_data["详情"]["ip"] = list(result_data["直播"].keys())
-    result_data["详情"]["iptv"] = sum(len(channels) for channels in result_data["直播"].values())
-    
-    # 覆盖写入 JSON 文件
-    write_json_file("data/itv.json", result_data, overwrite=True)
-
-def test_download_speed(url, test_duration=3, speed_threshold=0.3):
+def test_download_speed(url, test_duration=3):
     """
     测试下载速度，固定访问时间为 test_duration 秒，并加入速度阈值。
     如果下载速度低于阈值，返回 0。
@@ -282,12 +248,8 @@ def test_download_speed(url, test_duration=3, speed_threshold=0.3):
                 if elapsed_time >= test_duration:
                     break
 
-        speed = downloaded / test_duration if test_duration > 0 else 0  # 使用固定的 test_duration
-        speed_mb_s = round(speed / (1024 * 1024), 2)  # 转换为 MB/s 并保留两位小数
-
-        # 如果下载速度低于阈值，返回 0
-        if speed_mb_s < speed_threshold:
-            return 0
+        speed = downloaded / test_duration if test_duration > 0 else 0 
+        speed_mb_s = round(speed / (1024 * 1024), 2)  
 
         return speed_mb_s
 
@@ -317,14 +279,12 @@ def group_and_sort_channels(data):
     for ip, channel_list in data["直播"].items():
         channels.extend(channel_list)
 
-    # 定义分组
     groups = {
         '央视频道': [],
         '卫视频道': [],
         '其他频道': []
     }
 
-    # 将频道按名称分类
     for name, url, speed in channels:
         if 'cctv' in name.lower():
             groups['央视频道'].append((name, url, speed))
@@ -333,28 +293,21 @@ def group_and_sort_channels(data):
         else:
             groups['其他频道'].append((name, url, speed))
 
-    # 对每组进行排序
     for group in groups.values():
-        group.sort(key=lambda x: (
-            natural_key(x[0]),  # 名称自然排序
-            -x[2] if x[2] is not None else float('-inf')  # 速度从高到低排序
-        ))
+        group.sort(key=lambda x: ( natural_key(x[0]), -x[2] if x[2] is not None else float('-inf') ))
 
-    # 保存到 itvlist.txt 文件
     with open('itvlist.txt', 'w', encoding='utf-8') as file:
         for group_name, channel_list in groups.items():
             file.write(f"{group_name}:\n")
             for name, url, speed in channel_list:
                 file.write(f"{name},{url},{speed}\n")
-            file.write("\n")  # 打印空行分隔组
+            file.write("\n") 
 
-        # 添加当前时间的频道到“更新时间”分组
         current_time_str = datetime.now().strftime("%m-%d-%H")
         file.write(
             f"{current_time_str},#genre#:\n{current_time_str},https://git.3zx.top/https://raw.githubusercontent.com/MemoryCollection/IPTV/main/TB/mv.mp4\n"
         )
 
-    # 生成 itvlist.m3u 文件
     with open('itvlist.m3u', 'w', encoding='utf-8') as m3u_file:
         m3u_file.write('#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml"\n')
         for group_name, channel_list in groups.items():
@@ -362,9 +315,7 @@ def group_and_sort_channels(data):
                 m3u_file.write(
                     f'#EXTINF:-1 tvg-name="{name}" tvg-logo="https://git.3zx.top/https://raw.githubusercontent.com/MemoryCollection/IPTV/main/TB/{name}.png" group-title="{group_name}",{name}\n'
                 )
-                m3u_file.write(f"{url}\n")  # 只写入 URL，不带速度
-
-        # 添加当前时间的频道信息到 M3U 文件
+                m3u_file.write(f"{url}\n") 
         m3u_file.write(
             f'#EXTINF:-1 tvg-name="{current_time_str}" group-title="{current_time_str}", {current_time_str}\n'
         )
@@ -373,119 +324,62 @@ def group_and_sort_channels(data):
     print("分组后的频道信息已保存到 itvlist.txt 和 itvlist.m3u.")
     return groups
 
-def measure_download_speed_parallel(data):
+def measure_download_speed_parallel(data,MinSpeed = 0.3):
     """
-    并行测量多个 IP 地址下的频道下载速度，每个 IP 使用一个线程，
-    且每个线程内串行测试该 IP 下的频道。
-    空闲线程会自动获取新的任务，直到所有任务完成。
-    """
-    queue = Queue()
-    results = {"详情": {"ip": list(data.keys()), "iptv": sum(len(channels) for channels in data.values())}, "直播": {}}
-    max_threads = max(os.cpu_count() or 4, 4)
-
-    # 将数据添加到队列中
-    for ip, channels in data.items():
-        queue.put((ip, channels))
-
-    def worker():
-        thread_id = threading.current_thread().name  # 获取当前线程名称
-        while True:
-            try:
-                ip, channels = queue.get(timeout=1)  # 如果队列空，1秒后重试获取任务
-            except Empty:  # 如果队列空，退出线程
-                break
-
-            # 执行测速
-            channel_speeds = []
-            for index, (name, url, _) in enumerate(channels):
-                speed = test_download_speed(url)
-                speed = round(speed, 2)  # 转换速度为MB/s，并保留小数点后两位
-                channel_speeds.append([name, url, speed])  # 将测速结果添加到每个频道信息
-
-                # 按线程名称格式化进度输出
-                print(
-                    f"\r线程 {thread_id} 正在测试 IP {ip} 的频道 [{index + 1}/{len(channels)}] "
-                    f"总体进度 [{len(results['直播']) + 1}/{len(data)}]", 
-                    end=""
-                )
-            
-            # 将结果保存到 "直播" 字段
-            results["直播"][ip] = channel_speeds
-            queue.task_done()  # 标记任务完成
-
-    # 启动多线程
-    threads = []
-    for _ in range(max_threads):
-        thread = threading.Thread(target=worker)
-        thread.start()
-        threads.append(thread)
-
-    # 等待所有任务完成
-    queue.join()  # 阻塞主线程，直到所有任务都完成
-
-    for thread in threads:
-        thread.join()  # 等待所有线程执行完毕
-
-    # 覆盖写入文件
-    write_json_file("data/itv.json", results, overwrite=True)
-    return results
-
-def measure_download_speed_parallel(data):
-    """
-    并行测量多个 IP 地址下的频道下载速度，每个 IP 使用一个线程，
-    且每个线程内串行测试该 IP 下的频道。
-    空闲线程会自动获取新的任务，直到所有任务完成，确保至少有 6 个线程。
+    并行测量多个 IP 地址下的频道下载速度，
+    每个 IP 使用一个线程，且每个线程内串行测试该 IP 下的频道。
+    在测试频道速度之前，首先检查 IP 地址和端口是否可连接，
+    如果无法连接，则跳过该 IP 地址的测速。
+    MinSpeed 是最小速度。
     """
     queue = Queue()
-    results = {"详情": {"ip": list(data.keys()), "iptv": sum(len(channels) for channels in data.values())}, "直播": {}}
+    results = {"详情": {"iptv": sum(len(channels) for channels in data.values()), "ip": list(data.keys())}, "直播": {}}
     
     # 保证至少 6 个线程
     max_threads = max(os.cpu_count() or 4, 6)
 
-    # 将数据添加到队列中
     for ip, channels in data.items():
         queue.put((ip, channels))
 
     def worker():
-        thread_id = threading.current_thread().name  # 获取当前线程名称
+        thread_id = threading.current_thread().name  
         while True:
             try:
-                ip, channels = queue.get(timeout=1)  # 如果队列空，1秒后重试获取任务
-            except Empty:  # 如果队列空，退出线程
+                ip, channels = queue.get(timeout=1) 
+            except Empty:  
                 break
 
-            # 执行测速
+            if not check_ip_port(ip):
+                print(f"\r线程 {thread_id}: IP {ip} 端口无法连接，跳过测速", end="")
+                queue.task_done()  
+                continue   
+
             channel_speeds = []
             for index, (name, url, _) in enumerate(channels):
                 speed = test_download_speed(url)
-                speed = round(speed, 2)  # 转换速度为MB/s，并保留小数点后两位
-                channel_speeds.append([name, url, speed])  # 将测速结果添加到每个频道信息
+                speed = round(speed, 2)
+            
+                if speed > MinSpeed:
+                    channel_speeds.append([name, url, speed]) 
 
-                # 按线程名称格式化进度输出
-                print(
-                    f"\r线程 {thread_id} 正在测试 IP {ip} 的频道 [{index + 1}/{len(channels)}] "
-                    f"总体进度 [{len(results['直播']) + 1}/{len(data)}]", 
-                    end=""
-                )
+                    print(f"\r线程 {thread_id} 正在测试 IP {ip} 的频道 [{index + 1}/{len(channels)}] "
+                          f"总体进度 [{len(results['直播']) + 1}/{len(data)}]", end="")
 
-            # 将结果保存到 "直播" 字段
-            results["直播"][ip] = channel_speeds
-            queue.task_done()  # 标记任务完成
+            if channel_speeds:
+                results["直播"][ip] = channel_speeds
+            queue.task_done()
 
-    # 启动多线程
     threads = []
     for _ in range(max_threads):
         thread = threading.Thread(target=worker)
         thread.start()
         threads.append(thread)
 
-    # 等待所有任务完成
-    queue.join()  # 阻塞主线程，直到所有任务都完成
+    queue.join() 
 
     for thread in threads:
-        thread.join()  # 等待所有线程执行完毕
+        thread.join() 
 
-    # 覆盖写入文件
     write_json_file("data/itv.json", results, overwrite=True)
     return results
 
@@ -517,7 +411,6 @@ def upload_file_to_github(token, repo_name, file_path, folder='', branch='main')
     except Exception as e:
         print("文件上传失败:", e)
 
-
 if __name__ == "__main__":
 
     ip_data = read_json_file("data/itv.json")
@@ -538,8 +431,8 @@ if __name__ == "__main__":
 # json 保存数据格式
 # {
 #     "详情": {
-#         "ip": ["117.72.36.109:9099", "59.47.118.242:801", "60.16.18.243:4949"],
-#         "iptv": 6
+#         "iptv": 6,
+#         "ip": ["117.72.36.109:9099", "59.47.118.242:801", "60.16.18.243:4949"]
 #     },
 #     "直播": {
 #         "117.72.36.109:9099": [
