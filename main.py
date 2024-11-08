049492c50f82cfac628bf9308866cd51c2242da3
MemoryCollection <csszue@gmail.com>
2024-11-08 17:14:27 +0800

    😳忘记GitHub了

--- main.py
+++ main.py
@@ -489,6 +489,35 @@ def measure_download_speed_parallel(data):
     write_json_file("data/itv.json", results, overwrite=True)
     return results
 
+def upload_file_to_github(token, repo_name, file_path, folder='', branch='main'):
+    """
+    将结果上传到 GitHub，并指定文件夹
+    """
+    g = Github(token)
+    repo = g.get_user().get_repo(repo_name)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    git_path = f"{folder}/{file_path.split('/')[-1]}" if folder else file_path.split('/')[-1]
+
+    try:
+        contents = repo.get_contents(git_path, ref=branch)
+    except:
+        contents = None
+
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    try:
+        if contents:
+            repo.update_file(contents.path, current_time, content, contents.sha, branch=branch)
+            print("文件已更新")
+        else:
+            repo.create_file(git_path, current_time, content, branch=branch)
+            print("文件已创建")
+    except Exception as e:
+        print("文件上传失败:", e)
+
+
 if __name__ == "__main__":
 
     ip_data = read_json_file("data/itv.json")
